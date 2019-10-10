68b57e9775da347199f053f6ec22d897d2efcbd3
shirzlotnik <55185579+shirzlotnik@users.noreply.github.com>
2019-10-10 17:26:17 +0300

    Add files via upload

---
+++ MathOP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace ShirZN1
+{
+    public class MathOP
+    {
+        private int n1;
+        private int n2;
+        private char op;
+        private double res;
+
+        public MathOP(int nm1,int nm2,char opp)
+        {
+            this.n1 = nm1; this.n2 = nm2;this.op = opp;
+        }
+
+        public int GetN1() => this.n1;
+        public int GetN2() => this.n2;
+        public char GetOP() => this.op;
+        public double GetRes() => this.res;
+
+        public void SetN1(int x) => this.n1 = x;
+        public void SetN2(int x) => this.n2 = x;
+        public void SetOP(char x) => this.op = x;
+        public void SerRes(double x) => this.res = x;
+
+        public void FindRes()
+        {
+            switch(this.op)
+            {
+                case '+':
+                    this.res = this.n1 + this.n2 + 0.0;
+                    break;
+                case '-':
+                    this.res = this.n1 - this.n2 - 0.0;
+                    break;
+                case '*':
+                    this.res = this.n1 * this.n2 * 1.0;
+                    break;
+                case '/':
+                    if (this.n2 != 0)
+                        this.res = this.n1 / this.n2 * 1.0;
+                    break;
+
+            }
+        }
+
+        public string Stringi()
+        {
+            string s = " ";
+            s += this.n1 + " " + this.op + " " + this.n2 + "=" + this.res;
+            return s;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using NewUnit4;
+
+namespace ShirZN1
+{
+    class MainClass
+    {
+        public static void NegResults(Node<double>li)
+        {
+            Node<double> pos = li;
+            int count = 0;
+            while(pos!=null)
+            {
+                if (pos.GetValue() < 0.0)
+                    count++;
+                pos = pos.GetNext();
+            }
+            Console.WriteLine("There are {0} negative results",count);
+        }
+
+        public static void FindResults(Node<MathOP>li)
+        {
+            Node<MathOP> pos = li;
+            while(pos!=null)
+            {
+                pos.GetValue().FindRes();
+                pos = pos.GetNext();
+            }
+        }
+
+        public static Node<double> MakeResList(Node<MathOP>li)
+        {
+            Node<double> pos = new Node<double>(li.GetValue().GetRes());
+            li = li.GetNext();
+            while(li!=null)
+            {
+                pos = new Node<double>(li.GetValue().GetRes(), pos);
+                li = li.GetNext();
+            }
+            return pos;
+        }
+
+        public static Node<MathOP> DelByZero(Node<MathOP>li)
+        {
+            Node<MathOP> pos = li;
+            while ((pos.GetValue().GetN2() == 0) && (pos.GetValue().GetOP() == '/'))
+            {
+                pos = DelFirst(pos);
+                if (pos == null)
+                    return null;
+            }
+            while (pos.HasNext())
+            {
+                if ((pos.GetNext().GetValue().GetN2()) == 0 && (pos.GetNext().GetValue().GetOP() == '/'))
+                {
+                    pos.SetNext(pos.GetNext().GetNext());
+                }
+                pos = pos.GetNext();
+            }
+            return pos;
+        }
+
+        public static Node<MathOP> DelFirst(Node<MathOP>li)
+        {
+            li = li.GetNext();
+            return li;
+        }
+
+        public static Node<MathOP> Make_List()
+        {
+            Console.Write("Num1:");            int num1 = int.Parse(Console.ReadLine());            Console.Write("Num2:");            int num2 = int.Parse(Console.ReadLine());            Console.Write("OP:");            char op = char.Parse(Console.ReadLine());            MathOP mop1 = new MathOP(num1, num2, op);            Node<MathOP> li = new Node<MathOP>(mop1);
+            for (int i = 0; i < 2; i++)            {                Console.Write("Num1:");                num1 = int.Parse(Console.ReadLine());                Console.Write("Num2:");                num2 = int.Parse(Console.ReadLine());                Console.Write("OP:");                op = char.Parse(Console.ReadLine());                mop1 = new MathOP(num1, num2, op);                li = new Node<MathOP>(mop1, li);
+            }            return li;
+
+        }
+        public static void Main(string[] args)
+        {
+            Node<MathOP> li = Make_List();
+            li = DelByZero(li);
+            FindResults(li);
+            Node<double> li2 = MakeResList(li);
+            NegResults(li2);
+            
+        }
+    }
+}
